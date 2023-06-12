849d75f81b1110157749d59ff9748d669963ec51
huong1999 <huongyaya@gmail.com>
2023-06-12 16:27:48 +0700

    view child.  & view children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+
+import {
+  Component,
+  ViewChildren,
+  AfterViewInit,
+  QueryList,
+  ViewChild,
+} from '@angular/core';
+
+import { HelloComponent } from '../app/hello.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-viewchild';
+export class AppComponent implements AfterViewInit {
+  name = 'Angular';
+  @ViewChildren(HelloComponent) hellos: QueryList<any> | any;
+  @ViewChild(HelloComponent, { static: false }) hello1: HelloComponent | undefined;
+  ngAfterViewInit() {
+    this.hellos.forEach((hello: any) => console.log(hello));
+    console.log('Hello ', this.hello1);
+  }
 }
